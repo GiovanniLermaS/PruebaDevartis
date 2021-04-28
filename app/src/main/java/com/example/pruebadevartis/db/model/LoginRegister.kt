05a720c100710ad2f86966571a782a360aeb38a4
Giovanni Lerma Suarez <c-alerma@olimpiait.com>
2021-04-27 21:14:44 -0500

    Change names.

---
+++ app/src/main/java/com/example/pruebadevartis/db/model/LoginRegister.kt
@@ -0,0 +1,7 @@
+package com.example.pruebadevartis.db.model
+
+class LoginRegister {
+
+    var user: String? = null
+    var password: String? = null
+}
---
+++ app/src/main/java/com/example/pruebadevartis/db/model/ResponseLoginRegister.kt
@@ -0,0 +1,8 @@
+package com.example.pruebadevartis.db.model
+
+class ResponseLoginRegister {
+
+    var user_id: String? = null
+    var access_token: String? = null
+    var message: String? = null
+}
